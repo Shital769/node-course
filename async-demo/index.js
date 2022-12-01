6fd1791da54d46221210ac4aec52c2806c0f863b
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-12-02 01:15:10 +1100

    worked with callbacks in GitHub repositories

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,3 +1,24 @@
 console.log("Before");
+getUser(1, (user) => {
+  //get the reposoitories
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("Repos", repos);
+  });
+});
+
 console.log("After");
 
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user form a database...");
+    callback({ id: id, gitHubUsername: "Shital769" });
+  }, 2000);
+  return 1;
+}
+
+function getRepositories(username) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    return ["repo1", "repo2", "repo3"];
+  }, 2000);
+}
