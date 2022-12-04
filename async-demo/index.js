20733389c29895c3eac46db3c995744360f4498a
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-12-04 23:47:28 +1100

    worked with named function to rescue callback hell on async demo

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,24 +1,34 @@
+//ASYNCHRONOUS
 console.log("Before");
-getUser(1, (user) => {
-  //get the reposoitories
-  getRepositories(user.gitHubUsername, (repos) => {
-    console.log("Repos", repos);
-  });
-});
+
+//get user and getrepositories for that user
+getUser(1, getRepositories);
 
 console.log("After");
+
+//pass the user and when we have repos, we get commits for repos
+function getRepositories(user) {
+  getRepositories(user.gitHubUsername, getCommits);
+}
+// when we get commits for that repos, we display the commits
+function getCommits(repos) {
+  getCommits(repos, displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log(commits);
+}
 
 function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user form a database...");
     callback({ id: id, gitHubUsername: "Shital769" });
   }, 2000);
-  return 1;
 }
 
-function getRepositories(username) {
+function getRepositories(username, callback) {
   setTimeout(() => {
     console.log("Calling GitHub API...");
-    return ["repo1", "repo2", "repo3"];
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
