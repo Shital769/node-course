963e82579a9f8d4b0ab25b756c90d7650828b791
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-12-06 00:12:18 +1100

    worked on async and await approach as well as promised-based approach

--- async-demo/index.js
+++ async-demo/index.js
@@ -4,19 +4,33 @@ console.log("Before");
 //get user and getrepositories for that user
 // getUser(1, getRepositories);
 // or you can write above shotcut codes, they are same
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-    });
-  });
-});
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     });
+//   });
+// });
 
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repo) => getCommits(repo[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
+//Async and await approach
+async function displayCommits() {
+  try {
+
+    const user = await getUser(1);
+    const repo = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repo[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log("Error", err.message);
+  }
+}
+displayCommits();
+//Promise-based approach
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repo) => getCommits(repo[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
 
 console.log("After");
 
@@ -34,7 +48,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Calling GitHub API...");
-      resolve(["repo1", "repo2", "repo3"]);
+      // resolve(["repo1", "repo2", "repo3"]);
+      reject (new Error("cant get the repos"))
     }, 2000);
   });
 }
